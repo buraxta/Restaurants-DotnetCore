aa1c470f36e45d5d4104e0ec808c2ecd5720daef
buraxta <burakgulluler@gmail.com>
2024-12-19 21:06:14 +0300

    clean architecture

--- Restaurants.API/Controllers/IWeatherForecastService.cs
+++ Restaurants.API/Controllers/IWeatherForecastService.cs
@@ -3,6 +3,6 @@ namespace Restaurants.API.Controllers
 {
     public interface IWeatherForecastService
     {
-        IEnumerable<WeatherForecast> Get();
+        IEnumerable<WeatherForecast> Get(int result, int min, int max);
     }
 }
