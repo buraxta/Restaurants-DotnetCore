b65f5a05355eb01b9d9a3733dfcafb080bb74f38
buraxta <burakgulluler@gmail.com>
2024-12-22 17:30:50 +0300

    add-migration error

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    public RestaurantsDbContext()
+    {
+    }
+    public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options): base(options)
+    {
+         
+    }
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Adress);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+    }
+}
