d8f488bfc3b2f8883547adc056d4c180303bc956
buraxta <burakgulluler@gmail.com>
2024-12-30 11:23:23 +0300

    extract API services to extensions class

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middleware;
+using Serilog;
+using Serilog.Events;
+
+namespace Restaurants.API.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        } }
+                );
+            });
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+            builder.Host.UseSerilog((context, configuration) =>
+                configuration
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+                .WriteTo
+                .Console()
+            );
+        }
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,55 +2,21 @@ using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extensions;
 using Serilog;
-using Microsoft.Extensions.Configuration;
-using Serilog.Events;
 using Restaurants.API.Middleware;
 using Restaurants.Domain.Entities;
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        } }
-    );
-});
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 
+builder.AddPresentation();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog((context, configuration) =>
-    configuration
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-    .WriteTo
-    .Console()
-);
+
 
 
 var app = builder.Build();
