cd308805da55e8417df2f7885705da045f866e43
buraxta <burakgulluler@gmail.com>
2025-01-17 00:40:42 +0300

    add pagination validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿
+using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+    {
+        private int[] allowPagesizes = [5, 10, 15, 30];
+        public GetAllRestaurantsQueryValidator()
+        {
+            RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.PageSize)
+                .Must(x => allowPagesizes.Contains(x))
+                .WithMessage($"PageSize must be in [{string.Join(",", allowPagesizes)}]");
+        }
+    }
+}
